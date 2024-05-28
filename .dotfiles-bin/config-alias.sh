4ee5c110fa2130e703f0b067cd2b36ab14f6fc67
Jacob Bolton <j.m.bolton@gmail.com>
2024-05-27 20:23:15 -0400

    Fix conditional for zsh

--- .dotfiles-bin/config-alias.sh
+++ .dotfiles-bin/config-alias.sh
@@ -3,16 +3,16 @@
 (git --version | grep -i windows > /dev/null) && GIT_FOR_WINDOWS=1 || GIT_FOR_WINDOWS=0
 
 # Set up alias for config command, which manages the main configuration
-if [ $GIT_FOR_WINDOWS == 1 ]; then
+if [[ "$GIT_FOR_WINDOWS" == 1 ]]; then
   alias config='git --git-dir="$(cygpath -w "$HOME/.dotfiles/")" --work-tree="$(cygpath -w "$HOME")"'
 else
   alias config='git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 fi
 
 # Check if an overlay directory is present
-if [ -d "$HOME"/.dotfiles-overlay ]; then
+if [[ -d "$HOME"/.dotfiles-overlay ]]; then
   # Set up the config alias for maintaining the overlay directory
-  if [ $GIT_FOR_WINDOWS == 1 ]; then
+  if [[ "$GIT_FOR_WINDOWS" == 1 ]]; then
     alias config.='git --git-dir="$(cygpath -w "$HOME/.dotfiles-overlay/")" --work-tree="$(cygpath -w "$HOME")"'
   else
     alias config.='git --git-dir="$HOME/.dotfiles-overlay/" --work-tree="$HOME"'
