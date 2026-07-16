1989107fa1c350250f0c4704c2319db4a6045380
Jacob Bolton <j.m.bolton@gmail.com>
2026-07-15 23:25:12 -0400

    Add Microsoft example config
    See: https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/creating-profiles?view=powershell-7.6

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,3 +1,65 @@
+## Map PSDrives to other registry hives
+if (!(Test-Path HKCR:)) {
+    $null = New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+    $null = New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+}
+
+## Customize the prompt
+function prompt {
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+$prefix = if (Test-Path Variable:/PSDebugContext) { '[DBG]: ' } else { '' }
+    if ($principal.IsInRole($adminRole)) {
+        $prefix = "[ADMIN]:$prefix"
+    }
+    $body = 'PS ' + $PWD.path
+    $suffix = $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
+    "${prefix}${body}${suffix}"
+}
+
+## Create $PSStyle if running on a version older than 7.2
+## - Add other ANSI color definitions as needed
+
+if ($PSVersionTable.PSVersion.ToString() -lt '7.2.0') {
+    # define escape char since "`e" may not be supported
+    $esc = [char]0x1b
+    $PSStyle = [pscustomobject]@{
+        Foreground = @{
+            Magenta = "${esc}[35m"
+            BrightYellow = "${esc}[93m"
+        }
+        Background = @{
+            BrightBlack = "${esc}[100m"
+        }
+    }
+}
+
+## Set PSReadLine options and keybindings
+$PSROptions = @{
+    ContinuationPrompt = '  '
+    Colors             = @{
+        Operator         = $PSStyle.Foreground.Magenta
+        Parameter        = $PSStyle.Foreground.Magenta
+        Selection        = $PSStyle.Background.BrightBlack
+        InLinePrediction = $PSStyle.Foreground.BrightYellow + $PSStyle.Background.BrightBlack
+    }
+}
+Set-PSReadLineOption @PSROptions
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+
+## Add argument completer for the dotnet CLI tool
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+
 # Change to UTF-8 (with BOM in PowerShell 5.1, without BOM in 6+)
 # For Out-File, >, and >>
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' 
